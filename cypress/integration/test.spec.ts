a581bb1e29ac0fffc0b76049ac9fd8bc0612b7e4
Ian Brown <ian.brown@iress.com>
2020-08-07 12:31:17 +0100

    improve filter code

--- cypress/integration/test.spec.ts
+++ cypress/integration/test.spec.ts
@@ -1,9 +1,12 @@
 import domains from '../../out/domains.json';
 import { WebDomain } from '../../interfaces/web-domain';
 
+// TODO: load from file
+const approvedDomains = [3, 4];
+
 describe('Check home pages', () => {
 
-    filterApproved(domains as Array<WebDomain>).forEach(domain => {
+    (domains as Array<WebDomain>).filter(isApproved).forEach(domain => {
         const domainName = `https://${domain.name}`;
         it(domainName, ()=>  {
             cy.visit(domainName);
@@ -13,8 +16,6 @@ describe('Check home pages', () => {
 
 });
 
-function filterApproved(webDomains: WebDomain[]): WebDomain[] {
-    // TODO: load from file
-    const approvedDomains = [3, 4];
-    return webDomains.filter(domain => approvedDomains.some(approvedId => approvedId === domain.id));
+function isApproved(domain: WebDomain): boolean {
+    return approvedDomains.some(approvedId => approvedId === domain.id);
 }
